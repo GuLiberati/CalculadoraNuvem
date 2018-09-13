c46a6ada63e22176622cf4284aa1539a0008059a
GuLiberati <gustavolibersti@gmail.com>
2018-09-13 08:40:40 -0300

    Adicionado JSON

---
+++ src/main/java/br/com/db1/model/Produto.java
@@ -0,0 +1,21 @@
+package br.com.db1.model;
+
+public class Produto {
+	
+	private String nome;
+	private Integer quantidade;
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
