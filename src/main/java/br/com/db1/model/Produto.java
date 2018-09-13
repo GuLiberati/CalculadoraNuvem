e5d29183ebbac17cb023b7e0056b872261af0203
GuLiberati <gustavolibersti@gmail.com>
2018-09-13 09:35:33 -0300

    Projeto Calculadora Nuvem

--- src/main/java/br/com/db1/model/Produto.java
+++ src/main/java/br/com/db1/model/Produto.java
@@ -1,21 +1,44 @@
 package br.com.db1.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	@Column(nullable = false, name = "nome", length = 50)
 	private String nome;
+
+	@Column(nullable = false, name = "quantidade")
 	private Integer quantidade;
-	
-	public Integer getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(Integer quantidade) {
-		this.quantidade = quantidade;
-	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public String toString() {
+		return "Formando(a) DB1 Start: " + nome ;
+	}
+
 	
 }
